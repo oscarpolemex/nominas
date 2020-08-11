8798670c7c37250f507c1fd640af0b85ab0ed512
gis_a <gis_a@DESKTOP-CV8CQIO>
2020-08-11 12:14:09 -0500

    Migraciones y modificaciones para recibos

---
+++ database/factories/ReciboFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Recibo;
+use Faker\Generator as Faker;
+
+$factory->define(Recibo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_08_11_154954_create_recibos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecibosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recibos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('tipo_recibo_id')->comment('Fk de la tabla servidores publicos');
+            $table->foreign('tipo_recibo_id')->references('id')->on('tipos_recibos');
+            $table->year('anio');
+            $table->integer('consecutivo');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recibos');
+    }
+}
