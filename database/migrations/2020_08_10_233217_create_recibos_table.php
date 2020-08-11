5cfaf9a13f8d3d824f890cb4ed8880e4dff9b108
gis_a <gis_a@DESKTOP-CV8CQIO>
2020-08-11 12:47:43 -0500

    Correccion de llaves foraneas

--- database/migrations/2020_08_10_233217_create_recibos_table.php
+++ database/migrations/2020_08_10_233217_create_recibos_table.php
@@ -15,7 +15,7 @@ class CreateRecibosTable extends Migration
     {
         Schema::create('recibos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('tipo_recibo_id')->comment('Fk de la tabla servidores publicos');
+            $table->unsignedBigInteger('tipo_recibo_id')->comment('Fk de la tabla servidores publicos');
             $table->foreign('tipo_recibo_id')->references('id')->on('tipos_recibos');
             $table->year('anio');
             $table->integer('consecutivo');
