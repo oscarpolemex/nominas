c39dd4183d1d9631cc9fd4ad3c5bacb33602c761
oscar <oscarpolemex@gmail.com>
2021-09-10 14:06:35 -0500

    Recibo de nominas

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="https://polemex.gob.mx/IMG/logo_SAF_Legislatura_2.svg" class="logo" alt="{{config('app.name')}}">
+            <img src="https://polemex.gob.mx/IMG/logo_SAF_Legislatura_2.png" class="logo" alt="{{config('app.name')}}">
         </a>
     </td>
 </tr>
