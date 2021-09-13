f96ef1ff078b873b8f187b5a8755e603a7739a76
oscar <oscarpolemex@gmail.com>
2021-09-13 18:37:47 -0500

    Busquedas: cambio de filtros

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -75,6 +75,7 @@ class SolicitudController extends Controller
 //                        }
 //                    }
                     $token = $this->request->token;
+                    dd($token);
                     return view('solicitudes.recibos', compact('servidor', 'tiporecibo', 'token'));
                 } else {
                     return redirect()->back()->withErrors(["c_electronico" => "El correo electronico no coincide con el token asignado"]);
